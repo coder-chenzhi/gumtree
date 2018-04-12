d603515cdfe791aab2b536369290b108918193b7
coder.chenzhi <coder.chenzhi@gmail.com>
2018-04-12 23:56:41 +0800

    add one test case

--- demo/src/main/java/logchange/Demo.java
+++ demo/src/main/java/logchange/Demo.java
@@ -16,7 +16,8 @@ public class Demo {
 
     static Revision logChangeWithOtherLargeChurn = new Revision(
             "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\LinesOfCodeCalculator_7a89641.java",
-            "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\LinesOfCodeCalculator_10a8aa2.java"
+            "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\LinesOfCodeCalculator_10a8aa2.java",
+            "the result is not correct by using default parameter"
     );
 
     static Revision addOneLineOfLog = new Revision(
@@ -24,10 +25,22 @@ public class Demo {
             "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP_CommonPushCreditLoanTradeStatusStoreSpi_745aa91a78.java"
     );
 
+    static Revision addTwoLogWithOtherChurn = new Revision(
+            "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP3_AlipayO2oCreateOrderController_4cdb06ced.java",
+            "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP3_AlipayO2oCreateOrderController_bdbc36819.java"
+    );
+
     static Revision logStaticTextChange = new Revision(
             "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP3_CreditLoanAttributeProcessor_3b54451e5.java",
             "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP3_CreditLoanAttributeProcessor_12945c1eb.java",
             "two log statement static text change with other non-log statement change"
+    );
+
+    static Revision deleteLogWithOtherChurn = new Revision(
+            "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP3_JdbcBizOrderDaoForMysqlMainDb_07b566f15.java",
+            "E:\\Code\\GumTreeSpace\\gumtree\\test_data\\src_log_change\\TP3_JdbcBizOrderDaoForMysqlMainDb_7bf530e50.java",
+            "delete two log statement and some non-log statement, " +
+                    "the result is not correct by using default parameter"
     );
 
     public static void JDTParse(String file1, String file2) {
@@ -57,8 +70,8 @@ public class Demo {
         String file2 = test.getDst();
         ITree src;
         try {
-            src = new JdtTreeGenerator().setEncoding("GB2312").generateFromFile(file1).getRoot();
-            ITree dst = new JdtTreeGenerator().setEncoding("GB2312").generateFromFile(file2).getRoot();
+            src = new JdtTreeGenerator().setCharset("GB2312").generateFromFile(file1).getRoot();
+            ITree dst = new JdtTreeGenerator().setCharset("GB2312").generateFromFile(file2).getRoot();
             Matcher m = Matchers.getInstance().getMatcher(src, dst); // retrieve the default matcher
             m.match();
             ActionGenerator g = new ActionGenerator(src, dst, m.getMappings());
@@ -76,9 +89,9 @@ public class Demo {
 
 
     public static void runClient() {
-        Revision test = addOneLineOfLog;
+        Revision test = addTwoLogWithOtherChurn;
         String[] args = new String[]{
-                "-c", "gt.encoding.default", "GB2312",
+                "-c", "gt.charset.decoding", "UTF-8",
                 "webdiff", test.getSrc(), test.getDst()
         };
         Run.main(args);
